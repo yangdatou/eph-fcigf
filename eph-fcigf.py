b98ae60beffd2bc3c0c32778dc6d8e4ba5750c84
Junjie Yang <yangjunjie0320@gmail.com>
2023-08-09 00:37:00 -0700

    update

---
+++ eph-fcigf.py
@@ -0,0 +1,499 @@
+import sys
+from functools import reduce
+from typing import Callable, Tuple
+
+import numpy
+import scipy
+
+ElectronSpinNumber = Tuple[int, int]
+prod: Callable[[Tuple], int] = lambda x: reduce(lambda a, b: a * b, x)
+
+sys.path.append("/Users/yangjunjie/work/cc-eph/epcc-hol/")
+sys.path.append("/Users/yangjunjie/work/cc-eph/cqcpy-master/")
+
+MAX_MEMORY_MB = 2000  # MB
+MAX_MEMORY_GB = MAX_MEMORY_MB / 1024
+
+import pyscf
+from pyscf.fci.addons import des_a
+
+import epcc.fci
+from epcc.hol_model import HolModel
+from epcc.fci import contract_1e
+from epcc.fci import contract_ep_rspace as contract_ep
+from epcc.fci import contract_pp
+
+# import .lib
+from lib import gmres
+
+
+def gen_hop(t: numpy.ndarray, g: numpy.ndarray, w: numpy.ndarray,
+            nelec: ElectronSpinNumber = (1, 0), nph_max: int = 4) -> Callable:
+    r"""
+    This function generates the Hamiltonian operator in the Fock space:
+        :math:`H c_I |\Psi_I\rangle = s_{IJ} c_J |\Psi_J\rangle`
+
+    Args:
+        t: numpy.ndarray (nsite, nsite)
+            hopping matrix
+        g: numpy.ndarray (nmode, nsite, nsite)
+            electron-phonon coupling matrix
+        w: numpy.ndarray (nmode, )
+            phonon frequency matrix
+        nelec: ElectronSpinNumber
+            number of electrons
+        nph_max: int
+            maximum number of phonons
+
+    Returns:
+        hop: Callable
+            a function that takes a vector and returns the Hamiltonian vector product
+    """
+    nsite = t.shape[0]
+    nmode = g.shape[0]
+
+    assert t.shape == (nsite, nsite)
+    assert g.shape == (nmode, nsite, nsite)
+    assert w.shape == (nmode,)
+
+    shape = epcc.fci.make_shape(nsite, nelec, nmode, nph_max, e_only=False)
+
+    def hop(v):
+        c = v.reshape(shape)
+        hc = contract_1e(t, c, nsite, nelec, nmode, nph_max, e_only=False, space="r")
+        hc += contract_ep(g, c, nsite, nelec, nmode, nph_max)
+        hc += contract_pp(w, c, nsite, nelec, nmode, nph_max, xi=None)
+        return hc.reshape(-1)
+
+    return hop
+
+
+def build_hm(t: numpy.ndarray, g: numpy.ndarray, w: numpy.ndarray,
+             nelec: ElectronSpinNumber = (1, 0), nph_max: int = 4):
+    r"""
+    This function generates the Hamiltonian matrix in the Fock space:
+        H c_I |\Psi_I\rangle = s_{IJ} c_J |\Psi_J\rangle
+
+    Args:
+        t: numpy.ndarray (nsite, nsite)
+            hopping matrix
+        g: numpy.ndarray (nmode, nsite, nsite)
+            electron-phonon coupling matrix
+        w: numpy.ndarray (nmode, )
+            phonon frequency matrix
+        nelec: ElectronSpinNumber
+            number of electrons
+        nph_max: int
+            maximum number of phonons
+
+    Returns:
+        hm: numpy.ndarray (v_shape, v_shape)
+            Hamiltonian matrix
+    """
+    nsite = t.shape[0]
+    nmode = g.shape[0]
+
+    assert t.shape == (nsite, nsite)
+    assert g.shape == (nmode, nsite, nsite)
+    assert w.shape == (nmode,)
+
+    shape = epcc.fci.make_shape(nsite, nelec, nmode, nph_max, e_only=False)
+    size = prod(shape)
+
+    # h2e_ip = fci_obj.absorb_h1e(h1e, eri, norb, nelec_ip, fac=0.5)
+    if size * size * 8 / 1024 ** 2 > MAX_MEMORY_MB:
+        print("Required memory in MB: ", size * size * 8 / 1024 ** 2)
+        raise ValueError("Not enough memory for EPH-FCI Hamiltonian.")
+
+    hop = gen_hop(t, g, w, nelec=nelec, nph_max=nph_max)
+    hm = numpy.array([hop(v) for v in numpy.eye(size)])
+    return hm
+
+
+def solve_fci_slow(t: numpy.ndarray, g: numpy.ndarray, w: numpy.ndarray,
+                   nelec: ElectronSpinNumber = (1, 0), nph_max: int = 4,
+                   nroots: int = 1):
+    """
+    Solve the FCI problem using the slow method.
+
+    Args:
+        t: numpy.ndarray (nsite, nsite)
+            hopping matrix
+        g: numpy.ndarray (nmode, nsite, nsite)
+            electron-phonon coupling matrix
+        w: numpy.ndarray (nmode, )
+            phonon frequency matrix
+        nelec: ElectronSpinNumber
+            number of electrons
+        nph_max: int
+            maximum number of phonons
+
+    Returns:
+        e: numpy.ndarray (nroots, )
+            eigenvalues
+        v: numpy.ndarray (v_shape, nroots)
+            eigenvectors
+    """
+    hm = build_hm(t, g, w, nelec=nelec, nph_max=nph_max)
+    e, v = scipy.linalg.eigh(hm)
+    return e[:nroots], v[:, :nroots]
+
+
+def solve_fci(t: numpy.ndarray, g: numpy.ndarray, w: numpy.ndarray,
+              nelec: ElectronSpinNumber = (1, 0), nph_max: int = 4,
+              nroots: int = 1):
+    """
+    Solve the FCI problem using the fast method.
+
+    Args:
+        t: numpy.ndarray (nsite, nsite)
+            hopping matrix
+        g: numpy.ndarray (nmode, nsite, nsite)
+            electron-phonon coupling matrix
+        w: numpy.ndarray (nmode, )
+            phonon frequency matrix
+        nelec: ElectronSpinNumber
+            number of electrons
+        nph_max: int
+            maximum number of phonons
+
+    Returns:
+        e: numpy.ndarray (nroots, )
+            eigenvalues
+        v: numpy.ndarray (v_shape, nroots)
+            eigenvectors
+    """
+    shape = epcc.fci.make_shape(t.shape[0], nelec, g.shape[0], nph_max, e_only=False)
+    size = prod(shape)
+    hop = scipy.sparse.linalg.LinearOperator((size, size), matvec=gen_hop(t, g, w, nelec=nelec, nph_max=nph_max))
+
+    e, v = scipy.sparse.linalg.eigsh(hop, k=nroots, which="SA")
+    e, v = (e[0], v[:, 0]) if nroots == 1 else (e, v)
+    return e, v
+
+
+def dump_hol_model(hol_obj):
+    """
+    Dump the information from the HolModel object
+    to the format that can be used by the FCI code.
+
+    Args:
+        hol_obj: HolModel
+            The HolModel object.
+
+    Returns:
+        t: numpy.ndarray (nsite, nsite)
+            hopping matrix
+        g: numpy.ndarray (nmode, nsite, nsite)
+            electron-phonon coupling matrix
+        w: numpy.ndarray (nmode, )
+            phonon frequency matrix
+        nelec: ElectronSpinNumber
+            number of electrons
+    """
+
+    # The hopping matrix
+    t = hol_obj.tmatS()
+    # The electron-phonon coupling matrix
+    g = hol_obj.gmat[:, :hol_obj.L, :hol_obj.L].transpose(1, 2, 0)
+    # The phonon frequency matrix
+    w = numpy.ones([hol_obj.M]) * hol_obj.w
+
+    # The number of electrons
+    nelec = (hol_obj.na, hol_obj.nb)
+
+    return t, g, w, nelec
+
+def eph_fcigf_ip(hol_obj, omegas=None, ps=None, qs=None, nph_max=4, eta=0.01,
+                 conv_tol=1e-8, max_cycle=100, m=40, method="slow",
+                 verbose=0, stdout=sys.stdout):
+    """
+    Compute the electron-phonon Green's function using the FCI method.
+    NOTE: the number of electrons will be set to the order in which the
+    number of alpha electron is greater than or equal to the number of
+    beta electrons; the IP process will remove the alpha electron.
+
+    Parameters:
+        hol_obj : HolModel
+            The HolModel object.
+        omegas : float
+            Frequency for which the Green's function is computed.
+        nph_max : int, optional
+            Maximum number of phonons in the system.
+        ps : list of ints, optional
+            List of indices of the initial states.
+        qs : list of ints, optional
+            List of indices of the final states.
+        eta : float, optional
+            Broadening factor for numerical stability.
+        conv_tol : float, optional
+            Convergence tolerance for the FCI solver.
+
+    Returns:
+        gfns_ip : 3D array of complex floats, shape (nomega, np, nq)
+            The computed Green's function values.
+    """
+    log = pyscf.lib.logger.Logger(stdout, verbose)
+
+    t: numpy.ndarray
+    g: numpy.ndarray
+    w: numpy.ndarray
+    nelec: ElectronSpinNumber
+    t, g, w, nelec = dump_hol_model(hol_obj)
+
+    # For the IP problem, we need to remove one electron from the system
+    # make sure the alpha electron is greater or equal to the beta electron.
+    nelec = sorted(nelec)
+    assert nelec[0] >= nelec[1]
+
+    nsite: int
+    nmode: int
+    nsite = t.shape[0]
+    nmode = g.shape[0]
+
+    assert t.shape == (nsite, nsite)
+    assert g.shape == (nmode, nsite, nsite)
+    assert w.shape == (nmode,)
+
+    nelec_ip = (nelec[0] - 1, nelec[1])
+    assert nelec_ip[0] >= 0 and nelec_ip[1] >= 0
+
+    # Extract the information about the frequency and orbitals
+    if ps is None:
+        ps = numpy.arange(nsite)
+    ps = numpy.asarray(ps)
+    np = len(ps)
+
+    if qs is None:
+        qs = numpy.arange(nsite)
+    qs = numpy.asarray(qs)
+    nq = len(qs)
+
+    omegas = numpy.asarray(omegas)
+    nomega = len(omegas)
+
+    # Compute the FCI solution
+    ene_fci, v_fci = solve_fci(t, g, w, nelec=nelec, nph_max=nph_max, nroots=1)
+
+    # Set up the FCI Hamiltonian
+    shape = epcc.fci.make_shape(nsite, nelec, nmode, nph_max, e_only=False)
+    size = prod(shape)
+    hdiag = epcc.fci.make_hdiag(t, 0.0, g, w, nsite, nelec, nmode, nph_max, e_only=False, space='r')
+    assert v_fci.shape == (size,)
+    assert hdiag.shape == (size,)
+
+    ndet_alph, ndet_beta = shape[:2]
+    c_fci = v_fci.reshape(ndet_alph * ndet_beta, -1)
+
+    # Set up the IP manybody Hamiltonian
+    shape_ip = epcc.fci.make_shape(nsite, nelec_ip, nmode, nph_max, e_only=False)
+    size_ip = prod(shape_ip)
+    hdiag_ip = epcc.fci.make_hdiag(t, 0.0, g, w, nsite, nelec_ip, nmode, nph_max, e_only=False, space='r')
+    assert hdiag_ip.shape == (size_ip,)
+
+    # Build the RHS and LHS of the response equation
+    bps = [[pyscf.fci.addons.des_a(c, nsite, nelec, p) for p in ps] for c in c_fci.T]
+    eqs = [[pyscf.fci.addons.des_a(c, nsite, nelec, q) for q in qs] for c in c_fci.T]
+    bps = numpy.asarray(bps).transpose((1, 2, 3, 0)).reshape(np, size_ip)
+    eqs = numpy.asarray(eqs).transpose((1, 2, 3, 0)).reshape(nq, size_ip)
+
+    if method == "slow":
+        log.info("Solving the IP Green's function by the building the full FCI Hamiltonian.")
+        log.info("The size of the FCI Hamiltonian is %4.2f GB.", size_ip * size_ip * 8 / 1024 ** 3)
+
+        h_ip = build_hm(t, g, w, nelec_ip, nph_max)
+        assert h_ip.shape == (size_ip, size_ip)
+
+        def gen_gfn(omega):
+            omega_e0_eta_ip = omega - ene_fci - 1j * eta
+            h_ip_omega = h_ip + omega_e0_eta_ip * numpy.eye(size_ip)
+            xps = numpy.linalg.solve(h_ip_omega, bps.T).T
+            return numpy.dot(xps, eqs.T)
+
+    else:
+        log.info("Solving the IP Green's function.")
+
+        hop_ip = gen_hop(t, g, w, nelec_ip, nph_max)
+
+        def gen_gfn(omega):
+            omega_e0_eta_ip = omega - ene_fci - 1j * eta
+            hdiag_ip_omega = hdiag_ip + omega_e0_eta_ip
+
+            def h_ip_omega(v):
+                assert v.shape == (size_ip,)
+                hv_real = hop_ip(v.real)
+                hv_imag = hop_ip(v.imag)
+
+                hv = hv_real + 1j * hv_imag
+                hv += omega_e0_eta_ip * v
+
+                return hv.reshape((size_ip,))
+
+            xps = gmres(h_ip_omega, bs=bps, xs0=bps / hdiag_ip_omega,
+                        diag=hdiag_ip_omega, tol=conv_tol,
+                        max_cycle=max_cycle, m=m,
+                        verbose=verbose, stdout=stdout)
+
+            xps = xps.reshape(np, size_ip)
+            return numpy.dot(xps, eqs.T)
+
+    gfns_ip = numpy.asarray([gen_gfn(omega) for omega in omegas]).reshape((nomega, np, nq))
+    return gfns_ip
+
+def eph_fcigf_ea(hol_obj, omegas=None, ps=None, qs=None, nph_max=4, eta=0.01,
+                 conv_tol=1e-8, max_cycle=100, m=40, method="slow",
+                 verbose=0, stdout=sys.stdout):
+    """
+    Compute the electron-phonon Green's function using the FCI method.
+
+    Parameters:
+        hol_obj : HolModel
+            The HolModel object.
+        omegas : float
+            Frequency for which the Green's function is computed.
+        nph_max : int, optional
+            Maximum number of phonons in the system.
+        ps : list of ints, optional
+            List of indices of the initial states.
+        qs : list of ints, optional
+            List of indices of the final states.
+        eta : float, optional
+            Broadening factor for numerical stability.
+        conv_tol : float, optional
+            Convergence tolerance for the FCI solver.
+
+    Returns:
+        gfns_ea : 3D array of complex floats, shape (nomega, np, nq)
+            The computed Green's function values.
+    """
+    log = pyscf.lib.logger.Logger(stdout, verbose)
+
+    t: numpy.ndarray
+    g: numpy.ndarray
+    w: numpy.ndarray
+    nelec: ElectronSpinNumber
+    t, g, w, nelec = dump_hol_model(hol_obj)
+
+    nsite: int
+    nmode: int
+    nsite = t.shape[0]
+    nmode = g.shape[0]
+
+    assert t.shape == (nsite, nsite)
+    assert g.shape == (nmode, nsite, nsite)
+    assert w.shape == (nmode,)
+
+    # TODO: check this, it might be wrong
+    nelec_ea = _nelec_ea(nelec)
+    assert nelec_ea[0] >= 0 and nelec_ea[1] >= 0
+
+    # Extract the information about the frequency and orbitals
+    if ps is None:
+        ps = numpy.arange(nsite)
+    ps = numpy.asarray(ps)
+    np = len(ps)
+
+    if qs is None:
+        qs = numpy.arange(nsite)
+    qs = numpy.asarray(qs)
+    nq = len(qs)
+
+    omegas = numpy.asarray(omegas)
+    nomega = len(omegas)
+
+    # Solve the FCI problem
+    ene_fci, v_fci = solve_fci(t, g, w, nelec=nelec, nph_max=nph_max, nroots=1)
+
+    # Set up the FCI Hamiltonian
+    shape = epcc.fci.make_shape(nsite, nelec, nmode, nph_max, e_only=False)
+    size = prod(shape)
+    hdiag = epcc.fci.make_hdiag(t, 0.0, g, w, nsite, nelec, nmode, nph_max, e_only=False, space='r')
+    assert v_fci.shape == (size,)
+    assert hdiag.shape == (size,)
+
+    ndet_alph, ndet_beta = shape[:2]
+    c_fci = v_fci.reshape(ndet_alph * ndet_beta, -1)
+
+    # Set up the IP manybody Hamiltonian
+    shape_ea = epcc.fci.make_shape(nsite, nelec_ea, nmode, nph_max, e_only=False)
+    size_ea = prod(shape_ea)
+    hdiag_ea = epcc.fci.make_hdiag(t, 0.0, g, w, nsite, nelec_ea, nmode, nph_max, e_only=False, space='r')
+    assert hdiag_ea.shape == (size_ea,)
+
+    # Build the RHS and LHS of the response equation
+    bps = [[pyscf.fci.addons.des_a(c, nsite, nelec, p) for p in ps] for c in c_fci.reshape(ndet_alph * ndet_beta, -1).T]
+    eqs = [[pyscf.fci.addons.des_a(c, nsite, nelec, q) for q in qs] for c in c_fci.reshape(ndet_alph * ndet_beta, -1).T]
+    bps = numpy.asarray(bps).transpose((1, 2, 3, 0)).reshape(np, size_ea)
+    eqs = numpy.asarray(eqs).transpose((1, 2, 3, 0)).reshape(nq, size_ea)
+
+    if method == "slow":
+        log.info("Solving the IP Green's function by the building the full FCI Hamiltonian.")
+        log.info("The size of the FCI Hamiltonian is %4.2f GB.", size_ea * size_ea * 8 / 1024 ** 3)
+
+        h_ea = build_hm(t, g, w, nelec_ea, nph_max)
+        assert h_ea.shape == (size_ea, size_ea)
+
+        def gen_gfn(omega):
+            omega_e0_eta_ea = omega - ene_fci - 1j * eta
+            h_ea_omega = h_ea + omega_e0_eta_ea * numpy.eye(size_ea)
+            xps = numpy.linalg.solve(h_ea_omega, bps.T).T
+            return numpy.dot(xps, eqs.T)
+
+    else:
+        log.info("Solving the IP Green's function.")
+
+        hop_ea = gen_hop(t, g, w, nelec_ea, nph_max)
+
+        def gen_gfn(omega):
+            omega_e0_eta_ea = omega - ene_fci - 1j * eta
+            hdiag_ea_omega = hdiag_ea + omega_e0_eta_ea
+
+            def h_ea_omega(v):
+                assert v.shape == (size_ea,)
+                hv_real = hop_ea(v.real)
+                hv_imag = hop_ea(v.imag)
+
+                hv = hv_real + 1j * hv_imag
+                hv += omega_e0_eta_ea * v
+
+                return hv.reshape((size_ea,))
+
+            xps = gmres(h_ea_omega, bs=bps, xs0=bps / hdiag_ea_omega,
+                        diag=hdiag_ea_omega, tol=conv_tol,
+                        max_cycle=max_cycle, m=m,
+                        verbose=verbose, stdout=stdout)
+
+            xps = xps.reshape(np, size_ea)
+            return numpy.dot(xps, eqs.T)
+
+    gfns_ea = numpy.asarray([gen_gfn(omega) for omega in omegas]).reshape((nomega, np, nq))
+    return gfns_ea
+
+nsite = 4
+nmode = 4
+nelec = (1, 0)
+nph_max = 6
+conv_tol = 1e-6
+
+m = HolModel(
+    nsite, nmode, nelec[0] + nelec[1],
+    1.0, 0.1, bc='p', gij=None,
+    ca=numpy.eye(nsite), cb=None
+)
+m.na = 1
+m.nb = 0
+
+eta = 0.01
+ps = [0, 1]
+qs = [0, 1]
+omegas = numpy.linspace(-0.5, 0.5, 21)
+
+gf1_ip = eph_fcigf_ip(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, method="slow")
+gf2_ip = eph_fcigf_ip(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, conv_tol=conv_tol)
+err_ip = numpy.linalg.norm(gf1_ip - gf2_ip)
+assert err_ip < conv_tol
+
+# gf1_ea = eph_fcigf_ea_slow(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max)
+# gf2_ea = eph_fcigf_ea(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, conv_tol=conv_tol)
+# err_ea = numpy.linalg.norm(gf1_ea - gf2_ea)
+# assert err_ea < conv_tol
---
+++ lib.py
@@ -0,0 +1,150 @@
+import sys
+import typing
+from typing import Union, Optional, Callable
+
+import numpy
+from pyscf.lib import logger
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import gcrotmk
+
+'''
+GMRES/GCROT(m,k) for solving Green's function linear equations
+'''
+
+OptionalArray = Optional[numpy.ndarray]
+
+
+def _unpack(v=None, vs=None):
+    res = None
+
+    if v is not None and vs is None:
+        v = numpy.asarray(v)
+        res = v.reshape(1, -1) if v.ndim == 1 else None
+
+    if vs is not None and v is None:
+        vs = numpy.asarray(vs)
+        res = vs if vs.ndim == 2 else vs.reshape(1, -1) if vs.ndim == 1 else None
+
+    return res
+
+
+def gmres(h: (Callable | numpy.ndarray),
+          bs: OptionalArray = None, b: OptionalArray = None,
+          xs0: OptionalArray = None, x0: OptionalArray = None,
+          diag: OptionalArray = None,
+          m: int = 30, tol: float = 1e-6, max_cycle: int = 200,
+          verbose=0, stdout: typing.TextIO = sys.stdout) -> numpy.ndarray:
+    """Solve a linear system using the GMRES algorithm.
+
+    Solves the linear equation h x = b using the GMRES (Generalized Minimal Residual) algorithm.
+    GMRES is an iterative method for solving nonsymmetric linear systems by approximating the solution
+    in a Krylov subspace with minimal residual.
+
+    Args:
+        h (Union[Callable, numpy.ndarray]):
+            Either a callable representing the matrix-vector product operation or a matrix itself.
+        bs (ArrayLike):
+            Right-hand side vector(s). If multiple vectors are provided, they should be stacked vertically.
+        b (ArrayLike):
+            Deprecated. Equivalent to providing `bs`.
+        xs0 (ArrayLike, optional):
+            Initial guess for the solution. Defaults to `None`.
+        x0 (ArrayLike, optional):
+            Deprecated. Equivalent to providing `xs0`.
+        diag (ArrayLike, optional):
+            Diagonal preconditioner for the linear operator `h`. Defaults to `None`.
+        m (int, optional):
+            Number of Krylov basis vectors used in the GMRES algorithm. Defaults to 30.
+        tol (float, optional):
+            Tolerance for terminating the iteration. Defaults to 1e-6.
+        max_cycle (int, optional):
+            Maximum number of iterations. Defaults to 200.
+        verbose (int, optional):
+            Verbosity level. If greater than 0, detailed logs are printed. Defaults to 0.
+        stdout (typing.TextIO, optional):
+            Output stream for logging. Defaults to `sys.stdout`.
+
+    Returns:
+        numpy.ndarray:
+            Solution vector(s) of the linear system. If multiple right-hand side vectors are provided,
+            the solutions are stacked vertically.
+
+    Raises:
+        ValueError:
+            If convergence to tolerance is not achieved within the specified number of iterations.
+
+    Note:
+        The `b` and `x0` arguments are provided for compatibility.
+
+    """
+    log = logger.Logger(stdout, verbose)
+    bs = _unpack(b, bs)
+    xs0 = _unpack(x0, xs0)
+
+    assert bs is not None
+    nb, n = bs.shape
+    nnb = nb * n
+
+    assert diag.shape == (n,)
+
+    if callable(h):
+        def matvec(xs):
+            xs = numpy.asarray(xs).reshape(nb, n)
+            hxs = numpy.asarray([h(x) for x in xs]).reshape(nb, n)
+            return hxs.reshape(nnb, )
+
+        # noinspection PyArgumentList
+        hop = LinearOperator((nnb, nnb), matvec=matvec)
+    else:
+        def matvec(xs):
+            xs = numpy.asarray(xs).reshape(nb, n)
+            hxs = numpy.asarray([h.dot(x) for x in xs]).reshape(nb, n)
+            return hxs.reshape(nnb, )
+
+        assert h.shape == (n, n)
+
+        # noinspection PyArgumentList
+        hop = LinearOperator((nnb, nnb), matvec=matvec)
+
+    mop = None
+    if diag is not None:
+        diag = diag.reshape(-1)
+
+        def matvec(xs):
+            xs = numpy.asarray(xs).reshape(nb, n)
+            hxs = numpy.asarray([x / diag for x in xs]).reshape(nb, n)
+            return hxs.reshape(nnb, )
+
+        # noinspection PyArgumentList
+        mop = LinearOperator((nnb, nnb), matvec=matvec)
+
+    num_iter = 0
+
+    def callback(rk):
+        nonlocal num_iter
+        num_iter += 1
+        log.debug(f"GMRES: iter = {num_iter:4d}, residual = {numpy.linalg.norm(rk) / nb:6.4e}")
+
+    log.debug(f"\nGMRES Start")
+    log.debug(f"GMRES: nb  = {nb:4d}, n = {n:4d},  m = {m:4d}")
+    log.debug(f"GMRES: tol = {tol:4.2e}, max_cycle = {max_cycle:4d}")
+
+    if xs0 is not None:
+        xs0 = xs0.reshape(-1)
+
+    # noinspection PyTypeChecker
+    xs, info = gcrotmk(
+        hop, bs.reshape(-1), x0=xs0, M=mop,
+        maxiter=max_cycle, callback=callback, m=m,
+        tol=tol / nb, atol=tol / nb
+    )
+
+    if info > 0:
+        raise ValueError(f"Convergence to tolerance not achieved in {info} iterations")
+
+    if nb == 1:
+        xs = xs.reshape(n, )
+    else:
+        xs = xs.reshape(nb, n)
+
+    return xs
