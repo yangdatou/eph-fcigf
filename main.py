1ab19e66322147232236f00be123143cbe75915f
Junjie Yang <yangjunjie0320@gmail.com>
2023-08-09 20:33:38 -0700

    update

--- main.py
+++ main.py
@@ -9,12 +9,39 @@ from epcc.fci import contract_pp
 
 from ephfcigf import eph_fcigf_ip, eph_fcigf_ea
 
-def solve(omegas, log=sys.stdout):
+def solve(omegas, nph_max=10, m=None, log=sys.stdout, tmp=None):
+    eta = 0.01
+    gf1_ip = eph_fcigf_ip(m, omegas, ps=None, qs=None, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
+    # gf1_ea = eph_fcigf_ea(m, omegas, ps=None, qs=None, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
+    gf_fci = gf1_ip # + gf1_ea
+
+    for iomega, omega in enumerate(omegas):
+        s = - numpy.trace(gf_fci[:, :, iomega].imag) / numpy.pi
+        log.write("omega = %f, s = %f\n" % (omega, s))
+
+    return gf_fci
+
+if __name__ == '__main__':
+    import h5py
+    from pyscf.lib import chkfile
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    nomega_total = 32
+    nomega_per_node = nomega_total // size
+    assert nomega_per_node * size == nomega_total
+
+    log = "%s/log-%02d.out" % (os.environ['LOG_TMPDIR'], rank)
+    tmp = "%s/tmp-%02d.h5"  % (os.environ['LOG_TMPDIR'], rank)
+
     nsite = 4
     nmode = 4
     nelec = (1, 0)
 
-    nph_max  = 20
+    nph_max  = 2
     conv_tol = 1e-6
 
     m = HolModel(
@@ -24,39 +51,20 @@ def solve(omegas, log=sys.stdout):
     )
     m.na = 1
     m.nb = 0
-    #
-    eta = 0.01
-    ps = None # [0, 1, 2, 3]
-    qs = None # [0, 1, 2, 3]
-    gf1_ip = eph_fcigf_ip(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
 
-    # assert 1 == 2
-    # gf1_ea = eph_fcigf_ea(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, verbose=5, stdout=sys.stdout)
-    # gf_fci = gf1_ip + gf1_ea
-    #
-    # # gf2_ip = eph_fcigf_ip(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, method="slow", conv_tol=conv_tol, verbose=5, stdout=sys.stdout)
-    # # err_ip = numpy.linalg.norm(gf1_ip - gf2_ip)
-    # # assert err_ip < conv_tol
-    # #
-    # # gf2_ea = eph_fcigf_ea(m, omegas, ps=ps, qs=qs, eta=eta, nph_max=nph_max, method="slow", conv_tol=conv_tol, verbose=5, stdout=sys.stdout)
-    # # err_ea = numpy.linalg.norm(gf1_ea - gf2_ea)
-    # # assert err_ea < conv_tol
-    #
-    # for iomega, omega in enumerate(omegas):
-    #     s = - numpy.trace(gf_fci[:, :, iomega].imag) / numpy.pi
-    #     print("% 8.4f, %8.4f" % (omega, s))
-
-if __name__ == '__main__':
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    nomega_total = 64
-    nomega_per_node = nomega_total // size
-    assert nomega_per_node * size == nomega_total
-
-    log = open("%s/rank-%d.log" % (os.environ['LOG_TMPDIR'], rank), 'w')
     omegas = numpy.linspace(-0.5, 0.5, nomega_total)
-    solve(omegas[rank * nomega_per_node : (rank + 1) * nomega_per_node], log=log)
-    
+    tmp    = solve(
+        omegas[rank * nomega_per_node : (rank + 1) * nomega_per_node],
+        nph_max=nph_max, m=m, log=open(log, 'w'), tmp=h5py.File(tmp, 'w')
+        )
+
+    print(tmp.shape)
+    print("Rank %d finished." % rank)
+
+    gf_fci = comm.gather(tmp, root=0)
+
+    print(gf_fci)
+
+    if rank == 0:
+        gf = numpy.concatenate(gf_fci, axis=0)
+        print(gf.shape)
