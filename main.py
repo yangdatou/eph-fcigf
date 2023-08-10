28eb40737007b3f632b4042f68623debe78f3b6b
Junjie Yang <yangjunjie0320@gmail.com>
2023-08-09 20:39:36 -0700

    update

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from ephfcigf import eph_fcigf_ip, eph_fcigf_ea
 def solve(omegas, nph_max=10, m=None, log=sys.stdout, tmp=None):
     eta = 0.01
     gf1_ip = eph_fcigf_ip(m, omegas, ps=None, qs=None, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
-    # gf1_ea = eph_fcigf_ea(m, omegas, ps=None, qs=None, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
-    gf_fci = gf1_ip # + gf1_ea
+    gf1_ea = eph_fcigf_ea(m, omegas, ps=None, qs=None, eta=eta, nph_max=nph_max, verbose=5, stdout=log)
+    gf_fci = gf1_ip + gf1_ea
 
     for iomega, omega in enumerate(omegas):
         s = - numpy.trace(gf_fci[:, :, iomega].imag) / numpy.pi
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     size = comm.Get_size()
 
     nomega_total = 32
-    nomega_per_node = nomega_total // size
-    assert nomega_per_node * size == nomega_total
+    nomega = nomega_total // size
+    assert nomega * size == nomega_total
 
     log = "%s/log-%02d.out" % (os.environ['LOG_TMPDIR'], rank)
     tmp = "%s/tmp-%02d.h5"  % (os.environ['LOG_TMPDIR'], rank)
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     nmode = 4
     nelec = (1, 0)
 
-    nph_max  = 2
+    nph_max  = 5
     conv_tol = 1e-6
 
     m = HolModel(
@@ -54,17 +54,17 @@ if __name__ == '__main__':
 
     omegas = numpy.linspace(-0.5, 0.5, nomega_total)
     tmp    = solve(
-        omegas[rank * nomega_per_node : (rank + 1) * nomega_per_node],
+        omegas[rank * nomega : (rank + 1) * nomega],
         nph_max=nph_max, m=m, log=open(log, 'w'), tmp=h5py.File(tmp, 'w')
         )
 
-    print(tmp.shape)
+    assert tmp.shape == (nomega, nsite, nsite)
     print("Rank %d finished." % rank)
 
     gf_fci = comm.gather(tmp, root=0)
 
-    print(gf_fci)
-
     if rank == 0:
-        gf = numpy.concatenate(gf_fci, axis=0)
-        print(gf.shape)
+        gf_fci = numpy.concatenate(gf_fci, axis=0)
+        for iomega, omega in enumerate(omegas):
+            s = - numpy.trace(gf_fci[:, :, iomega].imag) / numpy.pi
+            print("omega = % 6.4f, s = % 6.4f" % (omega, s))
